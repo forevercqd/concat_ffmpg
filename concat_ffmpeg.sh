27b33ff9b6fce40a68a399bdb6b718a90614afee
Robert <robert@fengyifeideMac-mini.local>
2017-05-11 14:11:35 +0800

    1. 修改合并视频的视频编码时具体参数;

--- concat_ffmpeg.sh
+++ concat_ffmpeg.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+TARGET=merge.mp4
+ASECT_16_9=-vf "scale=-1:360,pad=640:ih:(ow-iw)/2" 
+
+rm -rf $TARGET 
+rm 0.mpg 1.mpg 2.mpg
+
+
+echo cqd ${SECT_16_9} 
 #ffmpeg -i 288X512_out.mp4 -i Eng_Output_1.mp4 -i 288X512_out.mp4 -filter_complex \
 #      "[0:v:0] [0:a:0]  [1:v:0] [1:a:0] [2:v:0] [2:a:0] concat=n=3:v=1:a=1 [v] [a]" \
 #         -map "[v]" -map "a" output_filter.mp4
@@ -15,22 +23,13 @@
 #    -map "[v]" -map "[a]"  output.mp4
 
 
-#ffmpeg  -i 288X512_out.mp4 -i Eng_Output_1.mp4 -i 288X512_out.mp4  -filter_complex \
-#            "[0]scale=288x512,setdar=16/9[a];[1]scale=288x512,setdar=16/9[b];[2]scale=288x512,setdar=16/9[c];[3]scale=288x512,setdar=16/9[d];[a][b][c][d]concat=n=4:v=1:a=1" output_new.mp4
-
-
-#ffmpeg -i ./0_start.mov -vcodec copy -acodec copy -vbsf h264_mp4toannexb 1.ts
-#ffmpeg -i ./1_middle.mov -vcodec copy -acodec copy -vbsf h264_mp4toannexb 2.ts
-#ffmpeg -i ./2_end.mov    -vcodec copy -acodec copy -vbsf h264_mp4toannexb 3.ts
-#ffmpeg -i "concat:1.ts|2.ts|3.ts" -acodec copy -vcodec copy -absf aac_adtstoasc output.mov
-
-
 # 目前测试仅该方法有效
-ffmpeg -i ./0_start.mov -r 30 -qscale 0 0.mpg    
-#ffmpeg -i ./1_middle.mp4 -vf scale=640:360 -aspect 16:9 -r 30 -b 1536K  -qscale 0 1.mpg
+ffmpeg -i   ./0_start.mov -r 30 -qscale 0 0.mpg   #片头 
+ffmpeg -i   ./2_end.mov   -r 30 -qscale 0 2.mpg   #片尾 
+ffmpeg -i   ./XingQiuJueQi_16_BFrames.mp4 -vf "scale=-1:360,pad=640:ih:(ow-iw)/2"  -r 30 -qscale 0 1.mpg 
+cat 0.mpg 1.mpg 2.mpg | ffmpeg -f mpeg -i - -vcodec h264 -r 30 -b 1536K -acodec aac -ab 60K  $TARGET
 
-ffmpeg -i 1024x768_43_test_shape.mp4  -vf "scale=-1:360,pad=640:ih:(ow-iw)/2"  -r 30 -b 1536K 1.mpg 
-ffmpeg -i ./2_end.mov -r 30   -qscale 0 2.mpg
-cat 0.mpg 1.mpg 2.mpg | ffmpeg -f mpeg -i - -vcodec mpeg4 out_new.mp4
-
-#ffmpeg -i 1024x768_43_test_shape.mp4  -vf "scale=-1:360,pad=640:ih:(ow-iw)/2" -r 30 -b 1536K  1024x768_43_640x360_169.mp4
+# 测试有音轨时视频合并,同时存在 4:3 转 16:9 转换，测试ok 
+#ffmpeg -i 1024x768_43_test_shape.mp4  -vf "scale=-1:360,pad=640:ih:(ow-iw)/2"  -r 30 -qscale 0 0.mpg 
+#ffmpeg -i XingQiuJueQi_16_BFrames.mp4 -vf "scale=-1:360,pad=640:ih:(ow-iw)/2"  -r 30 -qscale 0 1.mpg 
+#cat 0.mpg 1.mpg | ffmpeg -f mpeg -i - -vcodec h264 -r 30 -b 1536K -acodec aac -ab 60K  $TARGET
