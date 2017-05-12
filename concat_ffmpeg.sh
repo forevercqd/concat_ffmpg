7f444cd20e800864027d8d71b464b84769b98fdf
Robert <robert@fengyifeideMac-mini.local>
2017-05-12 12:00:09 +0800

    1. 上传修改过的脚本; 2. 需要进一步修改水印出现及消失的时间点;

--- concat_ffmpeg.sh
+++ concat_ffmpeg.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
 
 TARGET=merge.mp4
+
+
+SRC_FILE0=0_start
+SRC_FILE2=2_end
+SRC_FILE1=XingQiuJueQi_16_BFrames
+
 ASECT_16_9=-vf "scale=-1:360,pad=640:ih:(ow-iw)/2" 
 
 rm -rf $TARGET 
-rm 0.mpg 1.mpg 2.mpg
+rm -rf ${SRC_FILE0}.mpg ${SRC_FILE1}.mpg ${SRC_FILE2}.mpg
 
-SRCAME=0_start
-
-echo cqd ${SRCAME}.mov 
-echo cqd ${SECT_16_9} 
-#ffmpeg -i 288X512_out.mp4 -i Eng_Output_1.mp4 -i 288X512_out.mp4 -filter_complex \
-#      "[0:v:0] [0:a:0]  [1:v:0] [1:a:0] [2:v:0] [2:a:0] concat=n=3:v=1:a=1 [v] [a]" \
-#         -map "[v]" -map "a" output_filter.mp4
-
-
-#ffmpeg -i 0_start.mov -i 1_middle.mov -filter_complex \
-#      "[0:v:0] [0:a:0]  [1:v:0] [1:a:0] concat=n=2:v=1:a=1 [v] [a]" \
-#         -map "[v]" -map "[a]" output_filter.mp4
-
-
-#ffmpeg -i 288X512_out.mp4 -i Eng_Output_1.mp4\
-#    -filter_complex "[0:v:0] [0:a:0] [1:v:0] [1:a:0] concat=n=2:v=1:a=1 [v] [a]" \
-#    -map "[v]" -map "[a]"  output.mp4
-
-
-length=`ffmpeg -i  ./0_start_1.mp4 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,// `
-echo cqd length=$length
+length0=`ffmpeg -i ${SRC_FILE0}.mov 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,// `
+length1=`ffmpeg -i ${SRC_FILE1}.mov 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,// `
+echo cqd length=$length0 length1=$length1 
 
 # 目前测试仅该方法有效
-ffmpeg -i   ./0_start_new.mp4 -r 30 -qscale 0 0.mpg   #片头 
-ffmpeg -i   ./2_end.mov   -r 30 -qscale 0 2.mpg   #片尾 
-ffmpeg -i   ./XingQiuJueQi_16_BFrames.mp4 -vf "scale=-1:360,pad=640:ih:(ow-iw)/2"  -r 30 -qscale 0 1.mpg 
-cat 0.mpg 1.mpg 2.mpg | ffmpeg  -i -  -vf  "movie=mask.png,scale= -1: -1[watermask]; [in] [watermask] overlay=x='if(between(t, 2, 14), 50, -1000)':y=50 [out]"\
+ffmpeg -i ${SRC_FILE0}.mov  -r 30 -qscale 0 -y ${SRC_FILE0}.mpg  #片头 
+ffmpeg -i ${SRC_FILE2}.mov  -r 30 -qscale 0 -y ${SRC_FILE2}.mpg  #片尾 
+
+ffmpeg -i ${SRC_FILE1}.mp4  -vf "scale=-1:360,pad=640:ih:(ow-iw)/2"  -r 30 -qscale 0 -y ${SRC_FILE1}.mpg
+
+cat ${SRC_FILE0}.mpg ${SRC_FILE1}.mpg ${SRC_FILE2}.mpg | ffmpeg  -i -  \
+    -vf  "movie=mask.png,scale= -1: -1[watermask]; [in] [watermask] overlay=x='if(between(t, 2, 14), 50, -1000)':y=50 [out]"\
     -vcodec h264 -r 30 -b 1536K -acodec aac -ab 60K -y $TARGET 
 
 # 测试有音轨时视频合并,同时存在 4:3 转 16:9 转换，测试ok 
