84420a41dac7e6008317dc7c2b798641d8bd17e1
Robert <forevercqd@163.com>
2017-06-19 17:22:36 +0800

    1. 解决The encoder 'aac' is experimental 问题

--- concat_ffmpeg.sh
+++ concat_ffmpeg.sh
@@ -5,7 +5,7 @@ TARGET=merge.mp4
 
 SRC_FILE0=0_start
 SRC_FILE2=2_end
-SRC_FILE1=XingQiuJueQi_16_BFrames
+SRC_FILE1=1024x768_43_test_shape
 
 ASECT_16_9=-vf "scale=-1:360,pad=640:ih:(ow-iw)/2" 
 
@@ -19,12 +19,11 @@ echo cqd length=$length0 length1=$length1
 # 目前测试仅该方法有效
 ffmpeg -i ${SRC_FILE0}.mov  -r 30 -qscale 0 -y ${SRC_FILE0}.mpg  #片头 
 ffmpeg -i ${SRC_FILE2}.mov  -r 30 -qscale 0 -y ${SRC_FILE2}.mpg  #片尾 
-
 ffmpeg -i ${SRC_FILE1}.mp4  -vf "scale=-1:360,pad=640:ih:(ow-iw)/2"  -r 30 -qscale 0 -y ${SRC_FILE1}.mpg
 
 cat ${SRC_FILE0}.mpg ${SRC_FILE1}.mpg ${SRC_FILE2}.mpg | ffmpeg  -i -  \
     -vf  "movie=mask.png,scale= -1: -1[watermask]; [in] [watermask] overlay=x='if(between(t, 2, 14), 50, -1000)':y=50 [out]"\
-    -vcodec h264 -r 30 -b 1536K -acodec aac -ab 60K -y $TARGET 
+    -vcodec h264 -r 30 -b 1536K -acodec aac -strict -2  -ab 60K -y $TARGET 
 
 # 测试有音轨时视频合并,同时存在 4:3 转 16:9 转换，测试ok 
 #ffmpeg -i 1024x768_43_test_shape.mp4  -vf "scale=-1:360,pad=640:ih:(ow-iw)/2"  -r 30 -qscale 0 0.mpg 
